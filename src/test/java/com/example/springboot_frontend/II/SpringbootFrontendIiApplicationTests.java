1baee1a810c189553c3c29cfa7179b141be54c25
kitou88C39 <kanfutrooper@icloud.com>
2024-02-18 07:39:26 +0900

    first commit

---
+++ src/test/java/com/example/springboot_frontend/II/SpringbootFrontendIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_frontend.II;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFrontendIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
