0309d67480b23d8df23b801c0b3e30afbcb7b638
kitou88C39 <kanfutrooper@icloud.com>
2024-02-21 21:02:38 +0900

    java/com/example/springboot_frontendにcontrollerフォルダを作成し、 ClientForwardController.javaを作成する

--- src/main/java/com/example/springboot_frontend/Controller/ClientForwardController.java
+++ src/main/java/com/example/springboot_frontend/Controller/ClientForwardController.java
@@ -1,8 +1,13 @@
 package com.example.springboot_frontend.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientForwardController {
 
+    @GetMapping(value = "/**/{path:[~\\.]*}")
+    public String forward() {
+        return "forward:/";
+    }
 }
